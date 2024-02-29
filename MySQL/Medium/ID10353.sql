24b6bdde32ca84b8a6624561789760475579b437
pPark <paulpark003@gmail.com>
2024-02-29 00:38:52 -0500

    update medium

---
+++ MySQL/Medium/ID10353.sql
@@ -0,0 +1,11 @@
+-- Workers with the highest salaries
+
+-- https://platform.stratascratch.com/coding/10353-workers-with-the-highest-salaries?code_type=3
+
+-- Solution:
+
+select worker_title from (select t.worker_title, max(w.salary) over() as max_sal from title t
+inner join worker w 
+on t.worker_ref_id = w.worker_id) as final
+where w.salary = max_sal
+
