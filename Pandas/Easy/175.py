608a2b1261608041c295a3fa484b4e397ac51059
pPark <paulpark003@gmail.com>
2024-05-06 07:53:55 -0400

    Update 175.py

--- Pandas/Easy/175.py
+++ Pandas/Easy/175.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
+## Solution 1
+
 def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
     df_new = person.set_index('personId').join(address.set_index('personId'), on ="personId",how = "left")[['firstName', 'lastName', 'city', 'state']]
     return df_new 
+
+## Solution 2
+
+
+def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
+    comb = person.merge(address, left_on = "personId", right_on="personId", how="left")
+    comb = comb[["firstName","lastName","city","state"]]
+    return comb
